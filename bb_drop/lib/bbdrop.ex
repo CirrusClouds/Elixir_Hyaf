49cd4ee5b94fb1e3c7b3b39d6aefd3e0d13d9a35
JamieWaymark <jamie.walkerdine@waymark.tech>
2022-02-11 17:46:32 +0000

    Pipeline cleanup

--- bb_drop/lib/bbdrop.ex
+++ bb_drop/lib/bbdrop.ex
@@ -11,30 +11,26 @@ defmodule Bbdrop do
   end
 
   def call(conn, _opts) do
+    conn = conn
+    |> Plug.Conn.put_resp_header("Access-Control-Allow-Origin", "*") 
+    |> Plug.Conn.put_resp_header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, CREATE, HEAD, OPTIONS")
+    |> Plug.Conn.put_resp_header("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, Accept-Language, X-Authorization") 
     route(conn.method, conn.path_info, conn)
   end
 
   def route("OPTIONS", _path, conn) do
-    conn = Plug.Conn.put_resp_header(conn, "Access-Control-Allow-Origin", "*") 
-    conn = Plug.Conn.put_resp_header(conn, "Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, CREATE, HEAD, OPTIONS") 
-    conn = Plug.Conn.put_resp_header(conn, "Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, Accept-Language, X-Authorization") 
     conn  |> (Plug.Conn.send_resp 204, "")
     
   end
 
   def route("GET", ["recent_postings"], conn) do
     IO.puts "getting Recent Postings!"
-    conn = Plug.Conn.put_resp_header(conn, "Access-Control-Allow-Origin", "*") 
-    conn = Plug.Conn.put_resp_header(conn, "Allow", "GET, PUT, POST, DELETE, CREATE, HEAD"
-    )
     results = Bbdrop.Handlers.handle_get_recent_postings(conn)
     conn  |> (Plug.Conn.send_resp 200, results)
   end
 
   def route("POST", _path, conn) do
     IO.puts "Making a posting"
-    conn = Plug.Conn.put_resp_header(conn, "Access-Control-Allow-Origin", "*") 
-    conn = Plug.Conn.put_resp_header(conn, "Allow", "GET, PUT, POST, DELETE, CREATE, HEAD")
     Bbdrop.Handlers.handle_post_mood (Plug.Conn.read_body(conn))
   end
 
